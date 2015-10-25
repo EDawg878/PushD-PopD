df94b29798ba21016a9a9593124e82e2282b9c01
EDawg878 <EDawg878@gmail.com>
2015-10-25 13:52:24 -0500

    Fix notifications

--- Chrome Extension/background.js
+++ Chrome Extension/background.js
@@ -1,24 +1,26 @@
 
-function Notify(tab) {
-    chrome.notifications.create('PushD', {
-        type: 'list',
-        title: "My First Popup",
-        message: "My First Message",
-        iconUrl: 'icon_128.png',
-        items: [{ title: "PushD", message: tab.url}]
-     });
+function notify(title, message) {
+  var opt = {
+    type: "basic",
+    title: title,
+    message: message,
+    iconUrl: "save.png"
+  };
+  chrome.notifications.create("PushD", opt);
 }
+
 var stack = [];
+
 chrome.commands.onCommand.addListener(function(command) {
   chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
     var tab = tabs[0];
     if (command === "pushd") {
       stack.push(tab.url);
-      Notify(tab);
+      notify("Pushed URL", tab.url);
     } else if (command === "popd" && stack.length > 0) {
       var saved_url = stack.pop();
       chrome.tabs.update(tab.id, {url: saved_url});
-      Notify(tab);
+      notify("Popped URL", tab.url);
     }
   });
 });
